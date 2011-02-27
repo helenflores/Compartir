049624514d6712098cc0ae050db7d94c615b3e2b
Javier <javier@javier-HP-Compaq-6715b-KL428LA-ABM.(none)>
2011-02-27 15:54:26 -0600

    Algunas Vistas

--- app/controllers/nino_controller.rb
+++ app/controllers/nino_controller.rb
@@ -1,4 +1,5 @@
 class NinoController < ApplicationController
+protect_from_forgery :only => [:create, :update, :destroy]
 	def index
 		@ninos = Nino.all
 		respond_to do |format|
@@ -7,4 +8,32 @@ class NinoController < ApplicationController
 					  format.db  { send_data system "mysqldump -u compartir -pdatos AsocCompartir_development"}
 		end
 	end
+	
+
+	def show
+    
+  	end
+
+  	def create
+	@nino = Nino.new("codigo" => params[:code], "nombre"=> params[:nombre], "apellido" => params[:apellido], "estadoPadrinazgo" => params[:padrinazgo], "fechaNac" => params[:fecha], "sexo" => params[:sexo], "habitaCon" => params[:habita], "nombreCentro"=> params[:centro], "nombreSector" => [:sector], "codigoEducador" => params[:codigo])
+  	if @nino.save
+    	redirect_to nino_path @nino 
+  	else
+    	render :action => "new"
+  	end
+	end
+
+ 	 def edit
+  	end
+
+  	def update
+ 	end
+
+ 	def destroy
+  	end
+
+  	def new
+	
+  	end
+
 end
---
+++ app/controllers/padrino_controller.rb
@@ -0,0 +1,23 @@
+class PadrinoController < ApplicationController
+  def create
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def index
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def new
+  end
+
+end
